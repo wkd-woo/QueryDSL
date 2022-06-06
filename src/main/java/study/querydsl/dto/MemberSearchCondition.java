d218763c6ccfcddb3b418149d9a722c483a214a4
wkd-woo <wkdwoos@gmail.com>
2022-06-06 20:08:06 +0900

    순수 JPA와 QueryDsl

---
+++ src/main/java/study/querydsl/dto/MemberSearchCondition.java
@@ -0,0 +1,23 @@
+package study.querydsl.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberSearchCondition {
+    // 회원명, 팀명, 나이(ageGoe, ageLoe)
+
+    private String username;
+    private String teamName;
+    private Integer ageGoe;
+    private Integer ageLoe;
+
+    public MemberSearchCondition() {
+    }
+
+    public MemberSearchCondition(String username, String teamName, Integer ageGoe, Integer ageLoe) {
+        this.username = username;
+        this.teamName = teamName;
+        this.ageGoe = ageGoe;
+        this.ageLoe = ageLoe;
+    }
+}
